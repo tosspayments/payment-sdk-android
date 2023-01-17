3e59d4649b308b06514cd7256612d23631bf2993
Kangdroid <kangdroid@toss.im>
2023-01-17 14:21:18 +0900

    [PaymentWidget]
    - 결제 결과 복사하기 기능 추가

--- app/src/main/java/com/tosspayments/paymentsdk/sample/activity/PaymentResultActivity.kt
+++ app/src/main/java/com/tosspayments/paymentsdk/sample/activity/PaymentResultActivity.kt
@@ -1,8 +1,11 @@
 package com.tosspayments.paymentsdk.sample.activity
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
@@ -19,6 +22,7 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tosspayments.paymentsdk.sample.R
+import com.tosspayments.paymentsdk.sample.composable.CtaButton
 import com.tosspayments.paymentsdk.sample.composable.Label
 import com.tosspayments.paymentsdk.sample.composable.Title
 
@@ -38,44 +42,60 @@ class PaymentResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val isSuccess = intent?.getBooleanExtra(EXTRA_RESULT, false) == true
+        val resultDataList = intent?.getStringArrayListExtra(EXTRA_DATA).orEmpty()
+
         setContent {
-            Content()
+            Content(isSuccess, resultDataList)
         }
     }
 
     @Composable
-    fun Content() {
+    fun Content(isSuccess: Boolean, resultDataList: List<String>) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .background(Color.White)
-                .padding(24.dp, 12.dp, 24.dp, 12.dp)
-                .verticalScroll(rememberScrollState()),
+                .padding(24.dp, 12.dp, 24.dp, 12.dp),
             verticalArrangement = Arrangement.spacedBy(24.dp)
         ) {
-            ResultTitle()
-            Result()
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.spacedBy(24.dp)
+            ) {
+                ResultTitle(isSuccess)
+                Result(resultDataList)
+            }
+
+            if (isSuccess) {
+                val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+
+                CtaButton(text = "복사하기") {
+                    val copyData = StringBuilder()
+
+                    resultDataList.forEach { data ->
+                        copyData.appendLine(TextUtils.join(" : ", data.split("|")))
+                    }
+
+                    val clip = ClipData.newPlainText("Payment Result", copyData.toString())
+                    clipboard.setPrimaryClip(clip)
+                }
+            }
         }
     }
 
     @Composable
-    fun ResultTitle() {
-        Title(
-            "결제 ${
-                if (intent?.getBooleanExtra(
-                        EXTRA_RESULT,
-                        false
-                    ) == true
-                ) "성공" else "실패"
-            }",
-            Modifier.fillMaxWidth()
-        )
+    fun ResultTitle(isSuccess: Boolean) {
+        Title("결제 ${if (isSuccess) "성공" else "실패"}", Modifier.fillMaxWidth())
     }
 
     @Composable
-    fun Result() {
+    fun Result(resultDataList: List<String>) {
         Column(Modifier.fillMaxWidth(), verticalArrangement = Arrangement.spacedBy(12.dp)) {
-            intent?.getStringArrayListExtra(EXTRA_DATA)?.forEach { data ->
+            resultDataList.forEach { data ->
                 data.split("|").let {
                     ResultItem(it[0], it[1])
                 }
