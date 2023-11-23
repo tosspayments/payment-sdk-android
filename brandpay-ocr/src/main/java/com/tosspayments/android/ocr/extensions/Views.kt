fb84c1e7f67e1c68ab559390edcfc53594c1900a
JooYang <jh.yang@toss.im>
2023-11-23 15:22:25 +0900

    remove ocr

--- brandpay-ocr/src/main/java/com/tosspayments/android/ocr/extensions/Views.kt
+++
@@ -1,7 +0,0 @@
-package com.tosspayments.android.ocr.extensions
-
-import android.view.View
-
-internal fun View.setVisibility(isVisible: Boolean) {
-    visibility = if (isVisible) View.VISIBLE else View.GONE
-}
--- brandpay-ocr/src/main/java/com/tosspayments/android/ocr/model/ErrorCode.kt
+++
@@ -1,7 +0,0 @@
-package com.tosspayments.android.ocr.model
-
-enum class ErrorCode(val message: String) {
-    OCR_CANCELED("카드스캔 취소."),
-    OCR_INVALID("카드 스캔 실행을 위해서 카메라 권한이 필요합니다."),
-    OCR_FAILED("카드스캔 실패.")
-}
