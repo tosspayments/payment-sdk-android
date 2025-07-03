f2e817906692862cf5576b15b0052e238adf95ef
양주현 <jh.yang@toss.im>
2025-07-03 21:00:21 +0900

    feat(workspace): groovy -> kts 마이그레이션, Gradle 버전, AGP 버전, Kotlin 버전 및 샘플앱에서 쓰는 각종 라이브러리 버전 최신화

---
+++ app/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    namespace = "com.tosspayments.paymentsdk.sample"
+    compileSdk = 35
+
+    flavorDimensions += listOf("server", "source")
+
+    defaultConfig {
+        applicationId = "com.tosspayments.paymentsdk.sample"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = project.property("versionName") as String
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        missingDimensionStrategy("server", "live")
+        missingDimensionStrategy("source", "remote")
+    }
+
+    productFlavors {
+        create("dev") {
+            dimension = "server"
+            applicationIdSuffix = ".dev"
+        }
+        create("staging") {
+            dimension = "server"
+            applicationIdSuffix = ".staging"
+        }
+        create("live") {
+            dimension = "server"
+            applicationIdSuffix = ".live"
+        }
+        create("local") {
+            dimension = "source"
+        }
+        create("remote") {
+            dimension = "source"
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    buildFeatures {
+        compose = true
+        viewBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    add("localImplementation", project(":paymentsdk"))
+    add(
+        "remoteImplementation",
+        "com.github.tosspayments:payment-sdk-android:${project.property("versionName")}"
+    )
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.fragment.ktx)
+
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+
+    implementation(libs.bundles.compose)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
